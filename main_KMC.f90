8410d4125bd13318d441546aefb2823e3d71e660
Oscar Fajardo Fontiveros <oscarcapote@hotmail.es>
2017-04-25 12:07:18 +0200

    Arreglado DeltaE y hecho el proceso iterativo

--- main_KMC.f90
+++ main_KMC.f90
@@ -9,9 +9,9 @@ integer(8),dimension(:,:),allocatable :: geom,coord
 integer(8),dimension(:),allocatable :: S!Vector de spins
 real(8),dimension(:,:),allocatable :: pes!Gamma_{iT}
 real(8),dimension(:),allocatable :: phi!Gamma_{iT}
-real(8) :: gamma_iT
+real(8) :: gamma_iT,p_c
 real(8) :: r_c!radio de cut_off
-real(8)::beta,E,M,T,E2,W
+real(8)::beta,E,M,T,E2,W,EField,P,DeltaE,h_j,time,Dt
 
 T = get_db_arg(1,2.0d0)
 d = get_int_arg(2,int(2,8))
@@ -21,6 +21,8 @@ warmSteps = get_int_arg(5,int(1d3,8))
 measureSteps = get_int_arg(6,int(1,8))
 W = 1.0d0!Valor que acotara el potencial aletorio
 r_c = 10
+p_c = exp(-2.0d0*r_c)
+EField = 100.0d0
 
 !-------------------------------------------------------------------------------
 !-----------------------------Inicializacion------------------------------------
@@ -38,10 +40,12 @@ allocate(pes(NS,NS))
 
 
 call geometry_compute(geom,coord,NS,L,d)!Inicializamos geometria
+coord = coord+1
 
 S = (/(nint(grnd()), i=1,NS)/)*2-1!Inicializamos spins (-1 o 1)
 phi = (/(grnd(), i=1,NS)/)*2*W-W!Inicializamos potencial aleatorio
-gamma_iT = 0
+time = 0.0d0
+
 do i = 1, NS
   do j = 1, NS
     if(i==j)then
@@ -53,17 +57,110 @@ do i = 1, NS
     Dx = Dx-nint(dble(Dx)/dble(L))*L
     Dy = Dy-nint(dble(Dy)/dble(L))*L
     pes(i,j) = exp(-2*sqrt(Dx**2.0d0+Dy**2.0d0))
-    print*,i,j,  pes(i,j)
   end do
+  gamma_iT = sum(pes(i,:))
   pes(i,:) = pes(i,:)/sum(pes(i,:))
 end do
-do i = 1,NS
-  print*,i,pes(i,:)
-    print*,i,phi(i)
+call hamiltonian_polarization(S,phi,EField,coord,geom,NS,E,P)
+print*,t,E,P
+
+do itt=1,10000
+  Dt  = -log(grnd())/gamma_iT
+  t = t+Dt
+  call MC_step()
+  E = E +DeltaE
+  P = P + 2*(coord(j,1)-coord(i,1))
+  print*,t,E,P
 enddo
+
+
 contains
 
-!function hamiltonian(S,phi,x)
-!
-!endfunction
+subroutine hamiltonian_polarization(S,phi,EField,coord,geom,NS,E,P)
+      integer(8),dimension(:,:):: geom,coord
+      integer(8),dimension(:):: S!Vector de spins
+      real(8),dimension(:):: phi
+      real(8) :: E,EField,ESpin,P
+      integer(8) :: NS
+      integer(8) :: i,j
+
+      E = 0.0d0
+      P = 0.0d0
+      ESpin = 0.0d0
+
+      do i=1,NS,1
+        do j = 1, 4, 1
+          ESpin = ESpin+dble(S(i)*S(geom(i,j)))
+        end do
+        P = P + coord(i,1)*S(i)
+        E = E+phi(i)*S(i)
+      enddo
+      E = E+ESpin*0.5d0-EField*P
+endsubroutine
+
+function tower(pes) result(mu)
+  real(8),dimension(:) :: pes
+  integer(8) :: mu
+  real(8) :: suma,rnd
+  mu = 1
+  mu = 1!Reaccio qua pasara
+  rnd = grnd()
+  suma =pes(mu)
+  !print*,'Probabilitats',Pr
+  do while(rnd>suma)
+      mu = mu+1
+      suma = suma+pes(mu)
+  enddo
+end function
+
+subroutine MC_step()
+  !Seleccionamos celda ocupada al azar
+  do
+    i = floor(grnd()*NS)+1
+    if ( S(i)==1 ) then
+      exit
+    end if
+  enddo
+
+  !Seleccionmos al azar, una celda con distribucion exponencial
+  j = tower(pes(i,:))
+  !Si esta lleno, pasamos a la siguiente iteracion
+  if ( S(j)==1.or.pes(i,j).le.p_c ) then
+    return
+  end if
+
+  !Calculamos variacion de energí con cuidado que si i y j son
+  !primeros vecinos en la parte interaccion de primeros vecinos
+  !su interaccion no afectara al cambio de energia
+  DeltaE = 0.0d0
+  h_j = 0.0d0
+  do nn = 1, 4, 1
+    if(geom(i,nn)==j)then
+      cycle
+    endif
+    h_j = h_j + S(geom(i,nn))
+  end do
+  DeltaE = -h_j
+
+  h_j = 0.0d0
+  do nn = 1, 4, 1
+    if(geom(j,nn)==i)then
+      cycle
+    endif
+    h_j = h_j + S(geom(j,nn))
+  end do
+  DeltaE = DeltaE +h_j
+  DeltaE = 2.0d0*(DeltaE +phi(j)-phi(i)+EField*(coord(i,1)-coord(j,1)))!-2*(coord(i,1)-coord(j,1))*EField+E2
+
+  if(DeltaE<0.0d0)then
+      S(i) = -S(i)
+      S(j) = -S(j)
+  else if(grnd().le.exp(-beta*DeltaE))then
+      S(i) = -S(i)
+      S(j) = -S(j)
+  endif
+
+
+endsubroutine
+
 end program
